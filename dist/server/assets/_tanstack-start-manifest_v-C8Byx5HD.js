1e2d8c8fbbe47f11096d23326d7801d5bad98834
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-06-12 19:32:48 +0530

    Add vercel config

--- dist/server/assets/_tanstack-start-manifest_v-C8Byx5HD.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/LENOVO/Documents/GitHub/Dananjaya_Senevirathne/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BADa4rq3.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BADa4rq3.js" } }] }, "/": { filePath: "C:/Users/LENOVO/Documents/GitHub/Dananjaya_Senevirathne/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-Cnqpg7cQ.js"] } } });
-export {
-  tsrStartManifest
-};
