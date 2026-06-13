82450b75feab89697ca7fe6b6da9565a62e1e234
Dananjaya Senevirathne <dananjayasenevirathne674@gmail.com>
2026-06-13 12:41:00 +0530

    enable nitro for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  nitro: true,
   tanstackStart: {
-    server: { entry: "server" },
+    server: {
+      entry: "./src/server.ts",
+    },
   },
 });
